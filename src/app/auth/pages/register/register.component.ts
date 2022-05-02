0cc5a997f3efb464ec79ecc689d6473d87e98b53
JanPLosa <jplosa@tlsi.es>
2022-05-02 16:14:16 +0200

    Registro de usuarios

--- src/app/auth/pages/register/register.component.ts
+++ src/app/auth/pages/register/register.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
+
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -18,12 +21,25 @@ export class RegisterComponent  {
   });
 
   constructor( private fb: FormBuilder,
-               private router: Router ) { }
+               private router: Router,
+               private authService: AuthService ) { }
 
   registro() {
-    console.log( this.miFormulario.value );
+    
+    const { name, email, password } = this.miFormulario.value;
 
-    this.router.navigateByUrl('/dashboard')
+    this.authService.registro( name, email, password )
+      .subscribe( ok => {
+
+        if( ok === true ) {
+          this.router.navigateByUrl('/dashboard');
+        }
+        else {
+          // TODO mostrar mensaje de error
+          Swal.fire('Error', ok, 'error' )
+        }
+      });
+     
   }
   
 }
