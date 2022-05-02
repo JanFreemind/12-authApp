78683f0b4e4a5a0074aebfa829e4acd07af37544
JanPLosa <jplosa@tlsi.es>
2022-05-02 13:02:50 +0200

    Pantalla protegida

--- src/app/auth/pages/register/register.component.ts
+++ src/app/auth/pages/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -16,11 +17,13 @@ export class RegisterComponent  {
     password: ['123456', [ Validators.required, Validators.minLength(6) ]],
   });
 
-  constructor( private fb: FormBuilder ) { }
+  constructor( private fb: FormBuilder,
+               private router: Router ) { }
 
   registro() {
     console.log( this.miFormulario.value );
-    console.log( this.miFormulario.valid );
+
+    this.router.navigateByUrl('/dashboard')
   }
   
 }
