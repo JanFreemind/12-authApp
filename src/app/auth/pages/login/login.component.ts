ca93ee8a77c494d4ea5157c8faf5a79371c746a2
JanPLosa <jplosa@tlsi.es>
2022-05-02 15:08:36 +0200

    Mensajes de error visuales

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+
+import Swal from 'sweetalert2';
+
 import { AuthService } from '../../services/auth.service';
 
 @Component({
@@ -28,12 +31,13 @@ export class LoginComponent {
     
     this.authService.login( email, password )
       .subscribe( ok => {
-        //console.log(resp);
-        if( ok ) {
+      
+        if( ok === true ) {
           this.router.navigateByUrl('/dashboard');
         }
         else {
           // TODO mostrar mensaje de error
+          Swal.fire('Error', ok, 'error' )
         }
       });
 
